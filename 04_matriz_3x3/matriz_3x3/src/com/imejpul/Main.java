f3e55be570fda7260e2c44f97d422a878e1594d1
Unknown <ivanmejiapulido@gmail.com>
2017-11-22 20:52:46 +0100

    Ejercicio 4 resuelto

---
+++ 04_matriz_3x3/matriz_3x3/src/com/imejpul/Main.java
@@ -0,0 +1,39 @@
+package com.imejpul;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int MATRIZ_LINE_SIZE = 2;
+        final int MATRIZ_ROW_SIZE = 2;
+
+        int[][] matriz_numeros = new int[5][5];
+        for (int i = 0; i < MATRIZ_LINE_SIZE; i++) {
+            for (int j = 0; j < MATRIZ_ROW_SIZE; j++) {
+                matriz_numeros[i][j] = 0;
+            }
+        }
+
+        System.out.println("Introduce valores para matriz: ");
+
+        for (int i = 0; i < MATRIZ_LINE_SIZE; i++) {
+            for (int j = 0; j < MATRIZ_ROW_SIZE; j++) {
+                System.out.print("Valor para posición " + i + ", " + j + ": ");
+                matriz_numeros[i][j] = Integer.parseInt(br.readLine());
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < MATRIZ_LINE_SIZE; i++) {
+            for (int j = 0; j < MATRIZ_ROW_SIZE; j++) {
+                System.out.format("%d ", matriz_numeros[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
