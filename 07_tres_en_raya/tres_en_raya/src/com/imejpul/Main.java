112cb4710bd208c186aa7ae8d5ab32bb8a611c74
Unknown <ivanmejiapulido@gmail.com>
2017-12-04 19:25:11 +0100

    Ejercicio 7 Resuelto

--- 07_tres_en_raya/tres_en_raya/src/com/imejpul/Main.java
+++ 07_tres_en_raya/tres_en_raya/src/com/imejpul/Main.java
@@ -17,20 +17,68 @@ public class Main {
     }
 
 
-    public static int comprobarGanador(int tablero[][], int jugadas){
+    public static int comprobarGanador(int tablero[][]){
 
-        int tresRayaUno = 0;
-        int tresRayaDos = 0;
-        int ganador = 0;
+        //Filas
 
-        if (jugadas == 4){
-            System.out.println("¡EMPATE!");
+        if(tablero[0][0] == 1 && tablero[0][1] == 1 && tablero[0][2] == 1){
+            return 1;
+        }
+        if (tablero[0][0] == 2 && tablero[0][1] == 2 && tablero[0][2] == 2){
+            return  2;
         }
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                tablero[i][j] = 0;
-            }
+        if(tablero[1][0] == 1 && tablero[1][1] == 1 && tablero[1][2] == 1){
+            return 1;
+        }
+        if (tablero[1][0] == 2 && tablero[1][1] == 2 && tablero[1][2] == 2){
+            return  2;
+        }
+
+        if(tablero[2][0] == 1 && tablero[2][1] == 1 && tablero[2][2] == 1){
+            return 1;
+        }
+        if (tablero[2][0] == 2 && tablero[2][1] == 2 && tablero[2][2] == 2){
+            return  2;
+        }
+
+        //Columnas
+
+        if(tablero[0][0] == 1 && tablero[1][0] == 1 && tablero[2][0] == 1){
+            return 1;
+        }
+        if (tablero[0][0] == 2 && tablero[1][0] == 2 && tablero[2][0] == 2){
+            return  2;
+        }
+
+        if(tablero[0][1] == 1 && tablero[1][1] == 1 && tablero[2][1] == 1){
+            return 1;
+        }
+        if (tablero[0][1] == 2 && tablero[1][1] == 2 && tablero[2][1] == 2){
+            return  2;
+        }
+
+        if(tablero[0][2] == 1 && tablero[1][2] == 1 && tablero[2][2] == 1){
+            return 1;
+        }
+        if (tablero[0][2] == 2 && tablero[1][2] == 2 && tablero[2][2] == 2){
+            return  2;
+        }
+
+        //Diagonales
+
+        if(tablero[0][0] == 1 && tablero[1][1] == 1 && tablero[2][2] == 1){
+            return 1;
+        }
+        if (tablero[0][0] == 2 && tablero[1][1] == 2 && tablero[2][2] == 2){
+            return  2;
+        }
+
+        if(tablero[0][2] == 1 && tablero[1][1] == 1 && tablero[2][0] == 1){
+            return 1;
+        }
+        if (tablero[0][2] == 2 && tablero[1][1] == 2 && tablero[2][0] == 2){
+            return  2;
         }
 
         return 0;
@@ -42,7 +90,7 @@ public class Main {
 
         int fila;
         int columna;
-        int ganador = 0;
+        int ganador;
         int contadorJugadas = 0;
 
         System.out.println("---Tres en raya---");
@@ -72,8 +120,14 @@ public class Main {
             tablero[fila][columna] = 1;
             contadorJugadas++;
             visualizarTablero(tablero);
-            ganador = comprobarGanador(tablero, contadorJugadas);
-            if (ganador == 1 || contadorJugadas == 4) {
+            ganador = comprobarGanador(tablero);
+            if (ganador == 1){
+                System.out.println("El ganador es el jugador 1");
+                break;
+            }
+
+            if (contadorJugadas == 9){
+                System.out.println("¡EMPATE!");
                 break;
             }
 
@@ -91,8 +145,12 @@ public class Main {
             } while (columna < 0 || columna > 2);
 
             tablero[fila][columna] = 2;
+            contadorJugadas++;
             visualizarTablero(tablero);
-            ganador = comprobarGanador(tablero, contadorJugadas);
+            ganador = comprobarGanador(tablero);
+            if(ganador == 2){
+                System.out.println("El ganador es el jugador 2");
+            }
         } while (ganador == 0);
     }
 }
